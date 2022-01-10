d997fe1e86b2bc1b00cd1fd2d8fe8d1bbdbae477
Shuma Yoshioka <shuma_yoshioka@kiganix.co.jp>
2022-01-10 19:43:35 +0900

    Impl

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { Metadata, StringifyMarkdownOptions, VFM } from '@vivliostyle/vfm'
+import through2 from 'through2'
+import stream from 'stream'
+import type File from 'vinyl'
+import PluginError from 'plugin-error'
+
+const PLUGIN_NAME = 'gulp-vfm'
+
+export type GulpVfmPlugin = (vfmOptions?: StringifyMarkdownOptions, vfmMetadata?: Metadata) => stream.Transform
+
+const plugin: GulpVfmPlugin = (vfmOptions?, vfmMetadata?) => {
+  const processor = VFM(vfmOptions, vfmMetadata)
+
+  return through2.obj((chunk, enc, cb) => {
+    if (!isVinylFile(chunk)) {
+      return cb(new PluginError(PLUGIN_NAME, 'Invalid usages'))
+    }
+
+    const source = chunk.contents!!.toString()
+    const result = processor.processSync(source).toString()
+
+    chunk.contents = Buffer.from(result)
+    return cb(null, chunk)
+  })
+}
+
+function isVinylFile(chunk: any): chunk is File {
+  return 'isNull' in chunk && 'isStream' in chunk && 'contents' in chunk
+}
+
+export default plugin
